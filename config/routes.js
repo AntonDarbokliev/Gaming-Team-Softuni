075f93c60a33b13852ad75e5511698b9114db157
Anton Darbokliev <georgedarbokliev@gmail.com>
2023-10-18 15:51:02 +0300

    User model

--- config/routes.js
+++ config/routes.js
@@ -1,13 +1,13 @@
 const cookieParser = require("cookie-parser");
 const homeController = require("../controllers/homeController.js");
-// const userController = require("../controllers/userController.js");
+const userController = require("../controllers/userController.js");
 // const { auth } = require("../middlewares/authMiddleware.js");
 // const cryptoController = require("../controllers/cryptoController.js");
 module.exports = (app) => {
   app.use(cookieParser())
 //   app.use(auth)
   app.use(homeController);
-//   app.use("/user", userController);
+  app.use("/users", userController);
 //   app.use("/crypto", cryptoController);
 //   app.use('*',(req,res)=>{
 //     res.render('404')
